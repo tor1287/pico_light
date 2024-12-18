ef3e40ff7aa6776e6b56392e8b9279c9163ca21e
Tor kh <thanapolkhth@gmail.com>
2024-12-18 15:18:33 +0700

    interupt button handler

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-#set up onboard ws2812 on GP11
-
 import neopixel
+import time
 from machine import Pin
 
 light_pin = Pin(11, mode=Pin.OUT)
 light = neopixel.NeoPixel(light_pin, 2)
+button = Pin(20, mode=Pin.IN, pull=Pin.PULL_UP)
 
-light.fill((15, 15, 15))
-light.write()
+def light_red():
+    light.fill((15, 0, 0))
+    light.write()
+
+def light_green():
+    light.fill((0, 15, 0))
+    light.write()
+
+def light_blue():
+    light.fill((0, 0, 15))
+    light.write()
+
+def button_handler(button):
+    light.fill((15, 15, 15))
+    light.write()
+    time.sleep(1)
+
+button.irq(button_handler, trigger=button.IRQ_FALLING)
+
+while True:
+    light_red()
+    time.sleep(1)
+    light_green()
+    time.sleep(1)
+    light_blue()
+    time.sleep(1)
